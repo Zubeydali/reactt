04ab3b59a8b52db17fefe3915f49df97972d6e77
Zubeydali <160525888+Zubeydali@users.noreply.github.com>
2025-01-27 22:28:10 +0400

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../home/home.css'
+import Banner from '../../components/Banner/Banner'
+import Arrivals from '../../components/Arrivals/Arrivals'
+
+function Home() {
+  return (
+   <>
+
+   <Banner></Banner>
+   <Arrivals></Arrivals>
+   </>
+  )
+}
+
+export default Home
